f0e26daac9806e2c92eea7c5d4d555f3398d37f3
dev_janakalidasan <janakalidasan@rectoq.com>
2023-12-15 18:18:10 +0530

    laravel crud operation

---
+++ resources/views/students/create.blade.php
@@ -0,0 +1,24 @@
+@extends('students.layout')
+@section('content')
+ 
+<div class="card">
+  <div class="card-header">Students Page</div>
+  <div class="card-body">
+      
+      <form action="{{ url('student') }}" method="post">
+        {!! csrf_field() !!}
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" class="form-control"></br>
+        <label>Email</label></br>
+        <input type="email" name="email" id="email" class="form-control"></br>
+        <label>Password</label></br>
+        <input type="password" name="password" id="password" class="form-control"></br>
+        <label>Mobile</label></br>
+        <input type="text" name="mobile" id="mobile" class="form-control"></br>
+        <input type="submit" value="Save" class="btn btn-success"></br>
+    </form>
+   
+  </div>
+</div>
+ 
+@stop
